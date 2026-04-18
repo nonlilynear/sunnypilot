c82809a622093f2cbd5c673bf7be0822bc0505c6
ary <ary@hyperbeam.sh>
2026-04-17 20:23:52 -0700

    Remove duplicate radar init calls from sunnypilot interfaces (handled in opendbc)

--- sunnypilot/selfdrive/car/interfaces.py
+++ sunnypilot/selfdrive/car/interfaces.py
@@ -118,8 +118,6 @@ def setup_interfaces(CI: CarInterfaceBase, params: Params = None) -> None:
   nnlc_enabled = _initialize_neural_network_lateral_control(CP, CP_SP, params)
   _initialize_intelligent_cruise_button_management(CP, CP_SP, params)
   _initialize_torque_lateral_control(CI, CP, enforce_torque, nnlc_enabled)
-  _initialize_radar_tracks(CP, CP_SP, params)
-  _initialize_radar(CI, CP, CP_SP, params)
   _cleanup_unsupported_params(CP, CP_SP)
 
   try:
